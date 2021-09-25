e0e2db2660c215987abd8c58c25f303a499cd07a
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 11:20:22 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Our Project</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <script src="popup.js"></script>
+        <h1>The content</h1>
+    </body>
+</html>
