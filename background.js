4346266402615e35a0af304464e2ada3d12c0abe
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 12:17:56 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
 
+var tabs = 0;
+
+function callback(){
+    window.open("https://www.google.com/","_blank");
+    tabs +=1;
+    if (tabs == 10){
+        alert("disco party!!");
+    }
+};
+
+window.onload = function(){
+    chrome.tabs.onRemoved.addListener(callback);
+}
