e419b2bdd9f77b47a0f99bebc110d816495f634a
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 16:55:35 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
-
 var tabs = 0;
 
 function callback(){
     window.open("https://www.google.com/","_blank");
     tabs +=1;
     if (tabs == 10){
-        alert("disco party!!");
+        alert("DISCO PARTY!!!!!!!!")
+        window.open('assets/disco.gif', 'popup_name','height=' + screen.height + ',width=' + screen.width + ',resizable=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes')
+        tabs = 0;
     }
 };
 
 window.onload = function(){
+    tabs = 0;
     chrome.tabs.onRemoved.addListener(callback);
 }
