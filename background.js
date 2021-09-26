d30c1792d10274b2e923105e8765cd719fe99a6a
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 23:33:59 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
 var tabs = 0;
-var opened = 0;
+let closed = 0;
+var audio = new Audio("assets/msc.mp3");
 
 function callback(){
     choosePic();
     tabs +=1;
+    closed +=1;
+    audio.pause();
     if (tabs == 10){
         alert("DISCO PARTY!!!!!!!!")
         window.open('assets/disco.gif', 'popup_name','height=' + screen.height + ',width=' + screen.width + ',resizable=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes')
+        audio.play();
+        localStorage.setItem("vOneLocalStorage", closed); 
         tabs = 0;
     }
 };
@@ -33,3 +38,9 @@ function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
     window.open(myPix[randomNum],"_blank");
 }
+
+chrome.runtime.onMessage.addListener((message,_sender, sendResponse) => {
+    if (message === 'get-user-data'){
+        sendResponse(closed);
+    }
+});
