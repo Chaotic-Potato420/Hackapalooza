f45ab9c5957cddf7e252568813bed3b7abf2d38b
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 23:35:11 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
 
+chrome.runtime.sendMessage('get-user-data', (response) => {
+    console.log('received user data', response);
+    document.getElementById("tabClosed").innerHTML = "Tabs attempted to close today: " + response;
+})
