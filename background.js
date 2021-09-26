0ccc41f6a947a6ba54f8595822c9300d493a5c33
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2021-09-25 19:37:57 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 var tabs = 0;
+var opened = 0;
 
 function callback(){
-    window.open("https://www.google.com/","_blank");
+    choosePic();
     tabs +=1;
     if (tabs == 10){
         alert("DISCO PARTY!!!!!!!!")
-        window.open('disco.gif', 'popup_name','height=' + screen.height + ',width=' + screen.width + ',resizable=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes')
+        window.open('assets/disco.gif', 'popup_name','height=' + screen.height + ',width=' + screen.width + ',resizable=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes')
         tabs = 0;
     }
 };
@@ -13,4 +14,22 @@ function callback(){
 window.onload = function(){
     tabs = 0;
     chrome.tabs.onRemoved.addListener(callback);
+}
+
+var myPix = new Array(
+    "https://www.youtube.com/",
+    "https://www.google.com/",
+    "https://www.facebook.com/",
+    "https://twitter.com/",
+    "https://www.instagram.com/",
+    "https://www.boredbutton.com/",
+    "https://theuselessweb.com/",
+    "https://www.coolmathgames.com/",
+    "https://www.poptropica.com/",
+    "https://littlealchemy.com/"
+);
+
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    window.open(myPix[randomNum],"_blank");
 }
